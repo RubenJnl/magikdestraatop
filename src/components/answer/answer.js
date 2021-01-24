64b8db88333de7159043dbceb9ba77f95e038960
RubenJnl <ruben@ubenjanssen.nl>
2021-01-24 01:54:23 +0100

    Update counter

--- src/components/answer/answer.js
+++ src/components/answer/answer.js
@@ -12,56 +12,56 @@ const Answer = ({
 }) => {
     return (
       <>
-      {hond && (
-        <Styles.AnswerPositive>
-          Ja
-          <Styles.SubTitle>
-            , ook zodra de avondklok ingaat mag je met de hond op pad.
-          </Styles.SubTitle>
-        </Styles.AnswerPositive>
-      )}
-      {verklaring && (
-        <Styles.AnswerPositive>
-          Ja
-          <Styles.SubTitle>
-              , met een verklaring mag je tijdens de avondklok voor je werk op pad
-          </Styles.SubTitle>
-        </Styles.AnswerPositive>
-      )}
-      {eigenVerklaring && (
-        <Styles.AnswerPositive>
-          Ja
-          <Styles.SubTitle>
-              , met een geldige eigen verklaring mag je tijdens de avondklok op pad
-          </Styles.SubTitle>
-        </Styles.AnswerPositive>
-      )}
-      {bullshit && (
-        <Styles.AnswerNegative>
-          Nee
-          <Styles.SubTitle>
-            , we houden je niet tegen maar je bent gewaarschuwd!
-          </Styles.SubTitle>
-        </Styles.AnswerNegative>
-      )}
-      {!hond && !verklaring && !bullshit && !eigenVerklaring &&(
-        <>
-          {Switch() === 'night' && (
-            <Styles.AnswerNegative>
-                Nee
-            </Styles.AnswerNegative>
-          )}
-          {Switch() === 'day' && (
-            <Styles.AnswerPositive>
-              Ja
-              <Styles.SubTitle>
-                  , Voorlopig nog wel
-              </Styles.SubTitle>
-            </Styles.AnswerPositive>
-          )}
-        </>
-      )}
-      <Counter></Counter>
+        {hond && (
+          <Styles.AnswerPositive>
+            Ja
+            <Styles.SubTitle>
+              , ook zodra de avondklok ingaat mag je met de hond op pad.
+            </Styles.SubTitle>
+          </Styles.AnswerPositive>
+        )}
+        {verklaring && (
+          <Styles.AnswerPositive>
+            Ja
+            <Styles.SubTitle>
+                , met een verklaring mag je tijdens de avondklok voor je werk op pad
+            </Styles.SubTitle>
+          </Styles.AnswerPositive>
+        )}
+        {eigenVerklaring && (
+          <Styles.AnswerPositive>
+            Ja
+            <Styles.SubTitle>
+                , met een geldige eigen verklaring mag je tijdens de avondklok op pad
+            </Styles.SubTitle>
+          </Styles.AnswerPositive>
+        )}
+        {bullshit && (
+          <Styles.AnswerNegative>
+            Nee
+            <Styles.SubTitle>
+              , we houden je niet tegen maar je bent gewaarschuwd!
+            </Styles.SubTitle>
+          </Styles.AnswerNegative>
+        )}
+        {!hond && !verklaring && !bullshit && !eigenVerklaring &&(
+          <>
+            {Switch() === 'night' && (
+              <Styles.AnswerNegative>
+                  Nee
+              </Styles.AnswerNegative>
+            )}
+            {Switch() === 'day' && (
+              <Styles.AnswerPositive>
+                Ja
+                <Styles.SubTitle>
+                    , Voorlopig nog wel
+                </Styles.SubTitle>
+              </Styles.AnswerPositive>
+            )}
+          </>
+        )}
+        <Counter></Counter>
       </>
     )
 }
--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -2,8 +2,21 @@ import React, { useEffect, useState } from "react";
 import * as Style from './styles'
 function Counter() {
 
+  let [started, setStarted] = useState(false)
+  let diff = +new Date(2021, 0, 23, 21) - +new Date()
+
   const Calculate = () => {
-    const difference = +new Date(2021, 0, 23, 21) - +new Date();
+    let difference = +new Date(2021, 0, 23, 21) - +new Date()
+
+    if (difference < 0 ){
+      let counterDate = new Date()
+      counterDate.setHours(21)
+      counterDate.setMinutes(0)
+      counterDate.setSeconds(0)
+      counterDate.setDate(counterDate.getDate())
+      difference = +new Date(counterDate) - +new Date()     
+    }
+
     let timeLeft = {};
 
     if (difference > 0) {
@@ -15,8 +28,18 @@ function Counter() {
       };
     }
 
+    if ((timeLeft.hours === 6 && timeLeft.minutes >= 30) || timeLeft.hours > 6 ) {
+      timeLeft = false
+    }
+
     return timeLeft;
   };
+
+  useEffect(() => {
+    if (diff < 0 ){
+      setStarted(true)
+    }
+  })
 
   const [timeLeft, setTimeLeft] = useState(Calculate());
   
@@ -26,6 +49,7 @@ function Counter() {
     }, 1000);
   });
 
+
   return (
      <Style.Counter>
       {(timeLeft ? (
@@ -34,7 +58,12 @@ function Counter() {
           {timeLeft.uren === 1 ? ` ${timeLeft.hours} uur ` : ` ${timeLeft.hours} uur ` }
           {timeLeft.minutes === 1 ? ` ${timeLeft.minutes} minuut ` : ` ${timeLeft.minutes} minuten ` }
           {timeLeft.seconds === 1 ? ` ${timeLeft.seconds} seconde ` : ` ${timeLeft.seconds} seconden ` } <br />
-          tot de avondklok in gaat!
+          {(started ? (
+            <>tot de avondklok weer in gaat vanavond</>
+          ) : ( 
+            <>tot de avondklok in gaat!</> 
+          ) 
+          )}
         </>
       ) : '')}
     </Style.Counter>
